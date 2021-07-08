b28ede4fe51daac667b0e5e8706a6a88ccfab32b
ArlindKamberi <arlindkamberi03@gmail.com>
2021-07-08 15:50:58 +0200

    second attemp

--- my-first-app/src/App.js
+++ my-first-app/src/App.js
@@ -7,7 +7,9 @@ function App() {
     <div className={'content'}>
       <Title/>
       
-        <img src={logo} className="App-logo" alt="logo" />
+      <img alt="Arlind" src="http://www.portalishkollor.al/upload/media/posts/2021-04/23/arlind-kamberi-te-lexosh-libra-eshte-si-te-flasesh-me-me-te-miret-e-shekujve_1619188044-b.jpg"
+         width="170" height="160" align='center'>
+           </img>
         
     <p> I play classical and flamenco guitar since 11 years. </p>
     <p> I am really looking forward for this hybrid summer camp of CFA 2021</p>
